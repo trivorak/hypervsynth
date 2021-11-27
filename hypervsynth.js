985358f2b482a47df479a9afb04da5a977ce5e99
drake champagne <trivorak@gmail.com>
2021-11-27 12:25:04 -0600

    Added DelayLine Reverb
    Still testing but initial implimentation of Reverb

--- hypervsynth.js
+++ hypervsynth.js
@@ -33,13 +33,57 @@ fbGain.connect(delay);
 delayInput.connect(dryGain);
 dryGain.connect(delayOutput)
 
+// Reverb Unit
+
+// Init Modules
+var reverbInput = audioCtx.createGain();
+var reDelayL1 = audioCtx.createDelay();
+var reDelayL2 = audioCtx.createDelay();
+var reDelay1Gain = audioCtx.createGain();
+var reDelay2Gain = audioCtx.createGain();
+var reFeedback1Gain = audioCtx.createGain();
+var reFeedback2Gain = audioCtx.createGain();
+var passthru = audioCtx.createGain();
+var crossL1 = audioCtx.createGain();
+var crossL2 = audioCtx.createGain();
+var reverbOutput = audioCtx.createGain();
+//Connect Input -> Split two lines -> output gain -> output mixer (merger)
+reverbInput.connect(reDelayL1);
+reverbInput.connect(reDelayL2);
+reverbInput.connect(passthru);
+reDelayL1.connect(reDelay1Gain);
+reDelayL2.connect(reDelay2Gain);
+passthru.connect(reverbOutput);
+reDelay1Gain.connect(reverbOutput);
+reDelay2Gain.connect(reverbOutput);
+//Feedback Loops
+reDelay1Gain.connect(reFeedback1Gain);
+reDelay2Gain.connect(reFeedback2Gain);
+reFeedback1Gain.connect(reDelayL1);
+reFeedback2Gain.connect(reDelayL2);
+// Crossfeed lines
+reDelay1Gain.connect(crossL1);
+reDelay2Gain.connect(crossL2);
+crossL1.connect(reDelayL2);
+crossL2.connect(reDelayL1);
+// Reverb Defaults
+//----------------------------------------------------------
+// Setup Defaults to prevent crazy feedback
+reDelay1Gain.gain.value = 0.5;
+reDelay2Gain.gain.value = 0.5;
+reFeedback1Gain.gain.value = 0.5;
+reFeedback2Gain.gain.value = 0.5;
+reDelayL1.delayTime.value = 0.1;
+reDelayL2.delayTime.value = 0.333;
+
 
 // Conect Modules
 osc.forEach(element => element.connect(synthmixer));
 subOsc.connect(synthmixer);
 synthmixer.connect(lowpass);
 lowpass.connect(delayInput);
-delayOutput.connect(volume);
+delayOutput.connect(reverbOutput);
+reverbOutput.connect(volume);
 volume.connect(audioCtx.destination);
 
 
@@ -52,7 +96,9 @@ fbLowpass.type = "lowpass";
 fbLowpass.frequency.value = 250;
 lowpass.type = "lowpass";
 lowpass.frequency.value = (osc1.frequency.value + osc2.frequency.value + osc3.frequency.value + osc4.frequency.value)/4/2
-
+setReverbWet(0.75);
+setReverbTime(150);
+setReverbFeeback(0.95);
 
 //Spin up Oscillators
 osc.forEach(element => element.frequency.value = getRandomBetween(110,600));
@@ -62,6 +108,7 @@ osc.forEach(element => element.start());
 setSubOsc();
 subOsc.type = "square";
 subOsc.start();
+
 
 
 
@@ -114,3 +161,30 @@ function setDelayWet(wetValue){
 	dryGain.gain.value = 1 - wetValue;
 }
 
+
+//Reverb Functions
+//----------------------------------------------------------
+//Set Delay Wet&Dry Mix 
+function setReverbWet(wetValue){
+    if (wetValue > 1) {
+		wetValue = 1;
+	} 
+	if (wetValue < 0) {
+		wetValue = 0;
+	}
+	reDelay1Gain.gain.value = wetValue / 2;
+    reDelay2Gain.gain.value = wetValue / 2;
+    passthru.gain.value = 1 - wetValue;
+}
+
+//Set Delay Time based on a 1/13 prime number idea 
+function setReverbTime(inputTime){
+    reDelayL1.delayTime.value = inputTime * 1;
+    reDelayL2.delayTime.value = inputTime * 13;
+}
+
+function setReverbFeeback(feedback){
+    reFeedback1Gain.gain.value = feedback;
+    reFeedback2Gain.gain.value = feedback;
+}
+    
